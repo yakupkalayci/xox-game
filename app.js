5a64f540383d4bc2fa48a6291f35f7560b325020
yakupkalayci <ykpklyc18@gmail.com>
2022-08-19 00:15:32 +0300

    add logic for drawn

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let player = 1;
 let emptySquares = squares.length;
 let xs = [];
 let os = [];
+let isDrawn = true;
 
 const squaresObj = {
     s1: "",
@@ -42,13 +43,16 @@ function turnNotif(player) {
 }
 
 function initSquares() {
-    squares.forEach((square, index) => {
+    squares.forEach((square) => {
         square.addEventListener("click", () => drawLine(square));
     });
 }
 
 function drawLine(square) {
     if(emptySquares > 0) {
+        emptySquares--;
+        emptySquares && turnNotif(player);
+
         if(!square.textContent) {
             if(player === 1) {
                 square.textContent = "X";
@@ -70,14 +74,16 @@ function drawLine(square) {
                 }
             }
             player = player === 1 ? 2 : 1;
-            emptySquares--;
-            emptySquares && turnNotif(player);
         }
     }
 }
 
 function showWinnerAlert(winner) {
-    winnerAlert.innerHTML = `<p>${winner}. oyuncu kazandı. <br /> Tebrikler!</p><button>Yeniden Başlat</button>`
+    if(isDrawn) {
+        winnerAlert.innerHTML = `<p>Berabere!</p><button>Yeniden Başlat</button>`
+    } else {
+        winnerAlert.innerHTML = `<p>${winner}. oyuncu kazandı. <br /> Tebrikler!</p><button>Yeniden Başlat</button>`
+    }
     winnerAlert.style.display = "flex";
     const restartButton = document.querySelector(".winnerAlert > button");
     restartButton.addEventListener("click", restartGame);
@@ -88,47 +94,60 @@ function restartGame() {
 }
 
 function checkIsFinished(player) {
+    if(!emptySquares) {
+        if(isDrawn) {
+            showWinnerAlert();
+        }
+    }
     let {s1, s2, s3, s4, s5, s6, s7, s8, s9} = squaresObj;
     const squaresArr = Array.from(squares);
     if(player === 1) {
         if(s1 === "X" && s2 === "X" && s3 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "1" || square.id === "2"|| square.id === "3");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
         else if(s1 === "X" && s4=== "X" && s7 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "1" || square.id === "4"|| square.id === "7");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
         else if(s1 === "X" && s5=== "X" && s9 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "1" || square.id === "5"|| square.id === "9");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
         else if(s3 === "X" && s5=== "X" && s7 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "3" || square.id === "5"|| square.id === "7");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
         else if(s4 === "X" && s5=== "X" && s6 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "4" || square.id === "5"|| square.id === "6");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
         else if(s7 === "X" && s8=== "X" && s9 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "7" || square.id === "8"|| square.id === "9");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
         else if(s2 === "X" && s5=== "X" && s8 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "2" || square.id === "5"|| square.id === "8");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
         else if(s3 === "X" && s6=== "X" && s9 === "X") {
             const clickedSquares = squaresArr.filter(square => square.id === "3" || square.id === "6"|| square.id === "9");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(1);
         }
     }
@@ -136,41 +155,49 @@ function checkIsFinished(player) {
         if(s1 === "O" && s2 === "O" && s3 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "1" || square.id === "2"|| square.id === "3");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
         else if(s1 === "O" && s4=== "O" && s7 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "1" || square.id === "4"|| square.id === "7");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
         else if(s1 === "O" && s5=== "O" && s9 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "1" || square.id === "5"|| square.id === "9");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
         else if(s3 === "O" && s5=== "O" && s7 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "3" || square.id === "5"|| square.id === "7");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
         else if(s4 === "O" && s5=== "O" && s6 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "4" || square.id === "5"|| square.id === "6");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
         else if(s7 === "O" && s8=== "O" && s9 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "7" || square.id === "8"|| square.id === "9");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
         else if(s2 === "O" && s5=== "O" && s8 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "2" || square.id === "5"|| square.id === "8");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
         else if(s3 === "O" && s6=== "O" && s9 === "O") {
             const clickedSquares = squaresArr.filter(square => square.id === "3" || square.id === "6"|| square.id === "9");
             clickedSquares.forEach(square => square.style.color = "red");
+            isDrawn = false;
             showWinnerAlert(2);
         }
     }
